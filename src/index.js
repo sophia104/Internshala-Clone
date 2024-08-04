87a82fa80fbc96ed067f363106656f5c5ef59ab1
Aditi <81484760+sophia104@users.noreply.github.com>
2024-08-04 14:21:28 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+
+    <BrowserRouter>
+    <Auth0Provider
+    domain="dev-ky66u32nv7tbrh3r.us.auth0.com"
+    clientId="9DMy4PhxyxhBNc3n5vXAFPZWNgOsXYW5"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
